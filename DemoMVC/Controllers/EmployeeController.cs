a0966d2fe291692a8d2a7d50ef986ff0191681fb
jerome <zachariah11jerobert@gmail.com>
2023-11-24 10:04:33 +0530

    Initial MVC4 project upto controller

---
+++ DemoMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using DemoMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //
+        // GET: /Employee/
+
+        public ActionResult Details()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeId = 101,
+                Name = "John",
+                Gender = "Male",
+                City = "London"
+            };
+
+            return View(employee);
+        }
+
+    }
+}
---
+++ DemoMVC/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public string Index()
+        {
+            return "Hello from MVC Application";
+        }
+
+        public string GetDetails()
+        {
+            return "GetDetails() Invoked";
+        }
+
+        public List<string> CountryList()
+        {
+            return new List<string>() {
+                "India",
+                "US",
+                "UK",
+                "Canada"
+            };
+        }
+
+        public ViewResult Countries()
+        {
+            ViewBag.Countries=new List<string>() {
+                "India",
+                "US",
+                "UK",
+                "Canada"
+            };
+
+            return View();
+        }
+
+        public ViewResult Fruits()
+        {
+            ViewData["Fruits"] = new List<string>() {
+                "Apple",
+                "Banana",
+                "Grapes",
+                "Orange",
+                "Pomegranate"
+            };
+
+            return View();
+        }
+
+    }
+}
